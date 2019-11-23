4fa1fb06648d477e82e8a3c7749a9026a7688a76
NoppaGorn <tongnop2540@gmail.com>
2019-11-23 11:29:49 +0700

    first

--- src/test/java/ku/shop/BuyStepdefs.java
+++ src/test/java/ku/shop/BuyStepdefs.java
@@ -1,37 +1,13 @@
 package ku.shop;
 
-import io.cucumber.java.Before;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "classpath:features",
+                 plugin = {"pretty", "html:target/cucumber"})
+public class BuyUAT {
 
-public class BuyStepdefs {
-
-    private ProductCatalog catalog;
-    private Order order;
-
-    @Before
-    public void setup() {
-        catalog = new ProductCatalog();
-        order = new Order();
-    }
-
-    @Given("a product (.+) with price (.+) exists")
-    public void a_product_with_price_exists(String name, double price) {
-        catalog.addProduct(name, price);
-    }
-
-    @When("I buy (.+) with quantity (.+)")
-    public void i_buy_with_quantity(String name, int quant) {
-        Product prod = catalog.getProduct(name);
-        order.addItem(prod, quant);
-    }
-
-    @Then("total should be (.+)")
-    public void total_should_be(double total) {
-        assertEquals(total, order.getTotal());
-    }
 }
 
--- src/test/resources/features/buy.feature
+++ src/test/resources/features/buy.feature
@@ -5,14 +5,15 @@ Feature: Buy products
 Background:
     Given a product Bread with price 20.50 exists
     And a product Jam with price 80.00 exists
+    And a product snack with price 50.00 exists
+    And a product Milk with price 60.00 exists
 
 Scenario: Buy one product
-    When I buy Bread with quantity 2
-    Then total should be 41.00
+    When ฉันซื้อ milk with quantity 2
+    Then total should be 120.00
 
 Scenario: Buy multiple products
-    When I buy Bread with quantity 2
-    And I buy Jam with quantity 1
-    Then total should be 121.00
-
+    When ฉันซื้อ  Milk with quantity 2
+    And ฉันซื้อ snack with quantity 1
+    Then total should be 170.00
 
